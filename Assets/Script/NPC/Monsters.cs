cd64ad6bf6295ec4777804a1edb6934f1bc3a402
Ibrahim-Roy <ibrahimbadarroy@gmail.com>
2022-01-07 23:14:28 +0000

    Hostile NPC Bugs Fixed

--- Assets/Script/NPC/Monsters.cs
+++ Assets/Script/NPC/Monsters.cs
@@ -8,29 +8,30 @@ public class Monsters : HostileNPC
     {
         if(!attacking)
         {
-            attacking = true;
-            animator.SetTrigger("Attack");
-            StartCoroutine(damage());
+            StartCoroutine(attack());
         }
+    }
+
+    private IEnumerator attack()
+    {
+        
+        attacking = true;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(0.25f);
+        target.GetComponent<Player>().takeDamage(1);
+        yield return new WaitForSeconds(0.25f);
+        attacking = false;
         if(!backUp)
         {
             StartCoroutine(dodgeBack(stoppingDistance));
         }
     }
 
-    private IEnumerator damage()
-    {
-        yield return new WaitForSeconds(0.25f);
-        target.GetComponent<Player>().takeDamage(1);
-        yield return new WaitForSeconds(0.25f);
-        attacking = false;
-    }
-
     private IEnumerator dodgeBack(float distance)
     {
         backUp = true;
         stoppingDistance = (distance*2) + 0.3f;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         stoppingDistance = distance;
         backUp = false;
     }
